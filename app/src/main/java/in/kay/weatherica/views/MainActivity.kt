89b17c70e41ce6db865b5d7b8eb4c95b8cf09812
KapilYadav-dev <kapil1910086@akgec.ac.in>
2022-06-21 17:47:46 +0530

    Added CI/CD of Appcenter

--- app/src/main/java/in/kay/weatherica/views/MainActivity.kt
+++ app/src/main/java/in/kay/weatherica/views/MainActivity.kt
@@ -1,19 +1,31 @@
 package `in`.kay.weatherica.views
 
+import `in`.kay.weatherica.BuildConfig.APP_CENTER_KEY
 import `in`.kay.weatherica.ui.theme.WeathericaTheme
 import `in`.kay.weatherica.views.home.Home
+import android.os.Build
 import android.os.Bundle
 import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.annotation.RequiresApi
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.BuildConfig
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //Config app center for CI/CD of application
+        AppCenter.start(
+            application, APP_CENTER_KEY,
+            Analytics::class.java, Crashes::class.java
+        )
         window.setFlags(
             WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
             WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
@@ -23,11 +35,5 @@ class MainActivity : ComponentActivity() {
                 Home()
             }
         }
-    }
-
-    @Composable
-    @Preview
-    fun Preview() {
-        Home()
     }
 }
