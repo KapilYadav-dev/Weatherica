c16b1bf945165680356b5798cfe8de524cf1cd5d
KapilYadav-dev <kapil1910086@akgec.ac.in>
2022-06-21 17:39:16 +0530

    Done with improvement in UI

--- app/src/main/java/in/kay/weatherica/views/home/Home.kt
+++ app/src/main/java/in/kay/weatherica/views/home/Home.kt
@@ -5,6 +5,8 @@ import `in`.kay.weatherica.data.api.model.WeatherData
 import `in`.kay.weatherica.ui.theme.Hint
 import `in`.kay.weatherica.ui.theme.Poppins
 import `in`.kay.weatherica.ui.theme.Purple
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
@@ -21,23 +23,32 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
+import coil.request.CachePolicy
 import coil.request.ImageRequest
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
 
-@Preview
+
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun Home() {
     val homeViewModel = viewModel(modelClass = HomeViewModel::class.java)
-    val data = homeViewModel.getData("Delhi").collectAsState().value
-    if (data.days.isEmpty()) {
-        return
-    }
+    val data = homeViewModel.getData("Ranipur").collectAsState().value
+    val current = LocalDateTime.now()
+    val formatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM)
+    val date = current.format(formatter)
+    val day = LocalDate.now().dayOfWeek.name.toLowerCase().capitalize()
+
+
     ConstraintLayout(
         modifier = Modifier
             .background(Color.White)
@@ -52,22 +63,26 @@ fun Home() {
                 .fillMaxWidth()
                 .constrainAs(headerSection) {
                     top.linkTo(parent.top)
+                    bottom.linkTo(bottomSection.top, (-36).dp)
                     centerHorizontallyTo(parent)
+                    height = Dimension.fillToConstraints
                 }
         )
+
+
+
         ConstraintLayout(
             modifier = Modifier
                 .fillMaxWidth()
                 .constrainAs(bottomSection) {
-                    top.linkTo(headerSection.bottom, (-36).dp)
                     bottom.linkTo(parent.bottom)
-                    height = Dimension.fillToConstraints
+                    height = Dimension.wrapContent
                 }
                 .background(Purple, RoundedCornerShape(topStart = 26.dp, topEnd = 26.dp)),
         ) {
             val (tvTemp, tvCity, tvDate, tvPrediction, ivPrediction, viewDetails) = createRefs()
             Text(
-                text = data.days[0].temp.toInt().toString() + " °C",
+                text = data.currentConditions.temp!!.toInt().toString() + " °C",
                 fontSize = 48.sp,
                 color = Color.White,
                 fontFamily = Poppins,
@@ -94,9 +109,11 @@ fun Home() {
                 }
 
             )
-
             Text(
-                text = data.days[0].datetime,
+                text = "$day, $date ".let {
+                    var idx = it.lastIndexOf(',')
+                    it.removeRange(idx, it.length)
+                },
                 fontSize = 12.sp,
                 color = Color.White,
                 fontFamily = Poppins,
@@ -108,14 +125,15 @@ fun Home() {
 
             )
             Text(
-                text = data.days[0].conditions,
+                text = data.currentConditions.conditions!!,
                 fontSize = 18.sp,
                 color = Color.White,
                 fontFamily = Poppins,
                 fontWeight = FontWeight.SemiBold,
                 modifier = Modifier.constrainAs(tvPrediction) {
                     end.linkTo(parent.end, 24.dp)
-                    centerVerticallyTo(tvDate)
+                    bottom.linkTo(tvDate.bottom)
+                    top.linkTo(tvCity.top)
                 }
 
             )
@@ -124,13 +142,16 @@ fun Home() {
                 model = ImageRequest.Builder(LocalContext.current)
                     .data(getImage(data))
                     .crossfade(true)
+                    .crossfade(1000)
+                    .diskCachePolicy(CachePolicy.ENABLED)
                     .build(),
+                contentScale = ContentScale.Fit,
                 contentDescription = "",
                 modifier = Modifier
-                    .height(110.dp)
-                    .width(110.dp)
+                    .height(140.dp)
+                    .width(140.dp)
                     .constrainAs(ivPrediction) {
-                        bottom.linkTo(tvPrediction.top, 16.dp)
+                        bottom.linkTo(tvPrediction.top, 2.dp)
                         centerHorizontallyTo(tvPrediction)
                     }
             )
@@ -140,18 +161,18 @@ fun Home() {
                     top.linkTo(tvDate.bottom, 24.dp)
                     start.linkTo(parent.start, 24.dp)
                     end.linkTo(parent.end, 24.dp)
-                    bottom.linkTo(parent.bottom)
+                    bottom.linkTo(parent.bottom, 24.dp)
                     width = Dimension.fillToConstraints
-                    height = Dimension.fillToConstraints
+                    height = Dimension.wrapContent
                 }) {
                 Row(
                     horizontalArrangement = Arrangement.SpaceBetween,
                     modifier = Modifier.fillMaxWidth()
                 ) {
-                    Detail("Pressure", data.days[0].pressure.toString() + " hcpa")
-                    Detail("Precipitation", data.days[0].precip.toString() + " mm")
+                    Detail("Pressure", data.currentConditions.pressure.toString())
+                    Detail("Precipitation", data.currentConditions.precip.toString() + " mm")
 
-                    Detail("Humidity", data.days[0].humidity.toString() + " %")
+                    Detail("Humidity", data.currentConditions.humidity.toString() + " %")
                 }
                 Spacer(modifier = Modifier.height(32.dp))
                 Row(
@@ -159,8 +180,8 @@ fun Home() {
                     modifier = Modifier.fillMaxWidth()
                 ) {
                     Detail("Air Quality", "Good")
-                    Detail("Wind", data.days[0].windspeed.toString() + " km/h")
-                    Detail("Visibility", data.days[0].visibility.toString() + " km")
+                    Detail("Wind", data.currentConditions.windspeed.toString() + " km/h")
+                    Detail("Visibility", data.currentConditions.visibility.toString() + " km")
                 }
             }
         }
@@ -168,14 +189,15 @@ fun Home() {
     }
 }
 
-fun getImage(data: WeatherData): String? {
-    return when (data.days[0].icon) {
+fun getImage(data: WeatherData): String {
+    return when (data.currentConditions.icon) {
         "clear-day" -> "https://icons-for-free.com/iconfiles/png/512/sunny+temperature+weather+icon-1320196637430890623.png"
         "wind" -> "https://icons-for-free.com/iconfiles/png/512/storm+weather+wind+windy+icon-1320196635706326668.png"
         "rain" -> "https://icons-for-free.com/iconfiles/png/512/cloudy+forecast+rain+weather+icon-1320196634592211825.png"
         "snow" -> "https://icons-for-free.com/iconfiles/png/512/snow+weather+winter+icon-1320196635362560437.png"
         "cloudy" -> "https://icons-for-free.com/iconfiles/png/512/clouds+cloudy+weather+icon-1320196635828207342.png"
-        else -> "https://icons-for-free.com/iconfiles/png/512/temperature+thermometer+weather+icon-1320196636387608586.png"
+        "partly-cloudy-day" -> "https://icons-for-free.com/iconfiles/png/512/clouds+cloudy+weather+icon-1320196635828207342.png"
+        else -> ""
     }
 }
 
@@ -195,7 +217,8 @@ fun Detail(title: String, value: String) {
             color = Color.White,
             fontSize = 18.sp,
             fontFamily = Poppins,
-            fontWeight = FontWeight.Normal
+            fontWeight = FontWeight.Normal,
+            overflow = TextOverflow.Ellipsis
         )
     }
 }
--- app/src/main/java/in/kay/weatherica/views/home/HomeViewModel.kt
+++ app/src/main/java/in/kay/weatherica/views/home/HomeViewModel.kt
@@ -2,9 +2,11 @@ package `in`.kay.weatherica.views.home
 
 import `in`.kay.weatherica.data.api.model.WeatherData
 import `in`.kay.weatherica.data.repository.WeatherRepo
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -14,10 +16,15 @@ class HomeViewModel @Inject constructor(private val weatherRepo: WeatherRepo) : 
     private val _state = MutableStateFlow(WeatherData())
 
     fun getData(city:String):MutableStateFlow<WeatherData> {
-        viewModelScope.launch {
+        viewModelScope.launch(coroutineExceptionHandler) {
             val weather = weatherRepo.getWeather(city)
             _state.value = weather
         }
         return _state
+    }
+    private val coroutineExceptionHandler = CoroutineExceptionHandler{ _, t ->
+        run {
+            Log.d("MyerrorTag", "Error: " + t.localizedMessage)
+        }
     }
 }
